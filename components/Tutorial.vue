e1781280e2c5e4bbccf59307947d8653eb1e5fdc
MehedeeAlamAnon <mehedeealam@gmail.com>
2022-01-13 13:35:57 +0600

    first

---
+++ components/Tutorial.vue
@@ -0,0 +1,27 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div
+    class="relative min-h-screen bg-white-100 sm:items-center sm:pt-0"
+  >
+    <link
+      href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css"
+      rel="stylesheet"
+    />
+
+    <div>
+        <img :src="require('@/assets/icons/logo2.jpg')" class="w-50 mx-auto" />
+    </div>
+    <div class="mx-auto sm:px-6 lg:px-8">
+        <h2 class="text-7xl  font-bold text-center">
+          Welcome to SHANTA EQUITY
+        </h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial',
+}
+</script>
